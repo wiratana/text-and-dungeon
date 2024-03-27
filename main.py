3fc3ec562c56c6d3652d7c173f5442ae76810a48
wiratana <andikawiratana@gmail.com>
2024-03-27 21:47:22 +0800

    fix thread problem, and improve functionality

--- main.py
+++ main.py
@@ -14,54 +14,71 @@ def timestamp_to_time(timestamp):
 
 @bot.message_handler(commands=['display_data_inbox'])
 def display_data_inbox(message):
-    con = sqlite3.connect("db.sqlite3")
-    cur = con.cursor()
-    res = cur.execute("SELECT * FROM inbox ORDER BY id DESC")
-    inboxs = res.fetchall()
-    response = "\n----\n".join(["\n".join([str(property) for property in inbox]) for inbox in inboxs])
+    with sqlite3.connect("db.sqlite3") as con:
+        cur = con.cursor()
+        res = cur.execute("SELECT * FROM inbox ORDER BY id DESC")
+        inboxs = res.fetchall()
+        response = "\n----\n".join(["\n".join([str(property) for property in inbox]) for inbox in inboxs])
 
-    bot.reply_to(message, response)
+        bot.reply_to(message, response or 'tidak ada data gan')
 
 @bot.message_handler(commands=['display_data_outbox'])
 def display_data_outbox(message):
-    con = sqlite3.connect("db.sqlite3")
-    cur = con.cursor()
-    res = cur.execute("SELECT * FROM outbox ORDER BY id DESC")
-    inboxs = res.fetchall()
-    response = "\n----\n".join(["\n".join([str(property) for property in inbox]) for inbox in inboxs])
+    with sqlite3.connect("db.sqlite3") as con:
+        cur = con.cursor()
+        res = cur.execute("SELECT * FROM outbox ORDER BY id DESC")
+        inboxs = res.fetchall()
+        response = "\n----\n".join(["\n".join([str(property) for property in inbox]) for inbox in inboxs])
 
-    bot.reply_to(message, response)
+        bot.reply_to(message, response or 'tidak ada data gan')
 
-@bot.message_handler(commands=['hello'])
+@bot.message_handler(commands=['delete_all_data_inbox'])
+def delete_all_data_inbox(message):
+    with sqlite3.connect("db.sqlite3") as con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM inbox WHERE 1")
+        con.commit()
+        cur.close()
+        bot.reply_to(message, "inbox terhapus gan")
+
+@bot.message_handler(commands=['delete_all_data_outbox'])
+def delete_all_data_outbox(message):
+    with sqlite3.connect("db.sqlite3") as con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM outbox WHERE 1")
+        con.commit()
+        cur.close()
+        bot.reply_to(message, "outbox terhapus gan")
+
+@bot.message_handler(commands=['hello', 'start', 'hai', 'now'])
 def retrieve_message(message):
-    request = message
-    response = bot.reply_to(message, "yeyeyeye")
+    with sqlite3.connect("db.sqlite3") as con:
+        request = message
+        response = bot.reply_to(message, "yeyeyeye")
 
-    con = sqlite3.connect("db.sqlite3")
-    cur = con.cursor()
+        cur = con.cursor()
 
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS inbox(id INTEGER, usertoken INTEGER, created_at TEXT, username TEXT, message TEXT)")
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS outbox(id INTEGER, usertoken INTEGER, created_at TEXT, username TEXT, message TEXT)")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS inbox(id INTEGER, usertoken INTEGER, created_at TEXT, username TEXT, message TEXT)")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS outbox(id INTEGER, usertoken INTEGER, created_at TEXT, username TEXT, message TEXT)")
 
 
-    cur.execute("INSERT INTO inbox VALUES(?, ?, ?, ?, ?)",
-                (request.message_id,
-                 request.from_user.id,
-                 timestamp_to_time(request.date),
-                 request.from_user.username,
-                 request.text))
+        cur.execute("INSERT INTO inbox VALUES(?, ?, ?, ?, ?)",
+                    (request.message_id,
+                     request.from_user.id,
+                     timestamp_to_time(request.date),
+                     request.from_user.username,
+                     request.text))
 
-    cur.execute("INSERT INTO outbox VALUES(?, ?, ?, ?, ?)",
-                (response.message_id,
-                 response.from_user.id,
-                 timestamp_to_time(response.date),
-                 response.from_user.username,
-                 response.text))
+        cur.execute("INSERT INTO outbox VALUES(?, ?, ?, ?, ?)",
+                    (response.message_id,
+                     response.from_user.id,
+                     timestamp_to_time(response.date),
+                     response.from_user.username,
+                     response.text))
 
-    con.commit()
-    cur.close()
-    con.close()
+        con.commit()
+        cur.close()
 
 bot.infinity_polling()
